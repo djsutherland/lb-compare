f527bbf6070db4297a14479493335c856aa772be
Danica J. Sutherland <djs@djsutherland.ml>
2025-09-05 03:05:06 +0000

    add progress bars for fetching films

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,30 @@
 import streamlit as st
+
+from letterboxdpy.core.scraper import parse_url
 from letterboxdpy.user import User
+from letterboxdpy.pages.user_films import extract_movies_from_user_watched
+
+def grab_films(username):
+    text = f"Grabbing films for {username}"
+    pbar = st.progress(0.0, text=text)
+
+    user = User(username)
+    page = user.pages.films
+    first_dom = parse_url(f"{page.url}/page/1/")
+
+    movies = extract_movies_from_user_watched(first_dom)
+
+    pages = first_dom.find_all(class_='paginate-page')
+    n_pages = int(pages[-1].text) if pages else 1
+    pbar.progress(1 / n_pages, text=text)
+
+    for i in range(2, n_pages + 1):
+        dom = parse_url(f"{page.url}/page/{i}/")
+        movies.update(extract_movies_from_user_watched(dom))
+        pbar.progress(i / n_pages, text=text)
+    
+    return movies
+
 
 st.title("🎬 lb-compare")
 st.write("We'll look up movies that ")
@@ -9,13 +34,8 @@ un_to = st.text_input("letterboxd username", key="un_to")
 st.write("hasn't.")
 
 if un_from and un_to:
-    st.write(f"Grabbing {un_from}'s watched movies...")
-    u_from = User(un_from)
-    m_from = u_from.get_films()['movies']
-
-    st.write(f"Grabbing {un_to}'s watched movies...")
-    u_to = User(un_to)
-    m_to = u_to.get_films()['movies']
+    m_from = grab_films(un_from)
+    m_to = grab_films(un_to)
 
     cands = {k: v for k, v in m_from.items() if k not in m_to}
     st.write(f"{len(cands):,} candidate movies:")
